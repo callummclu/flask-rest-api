865fe2e3a65acebe3fee85e6ffde75c1e7adffb6
Callum <callummcluskey100@gmail.com>
2022-08-21 16:46:09 +0100

    added get and posts routes

--- rest-api/index.py
+++ rest-api/index.py
@@ -1,8 +1,21 @@
 from json import JSONEncoder
-import os
 import psycopg2 as db
-from flask import Flask
+from flask import Flask,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+db_sql = SQLAlchemy(app)
+
+class Post(db_sql.Model):
+    id = db_sql.Column(db_sql.Integer, primary_key=True)
+    title = db_sql.Column(db_sql.String(255))
+    content = db_sql.Column(db_sql.String(255))
+
+    def __init__(self,title,content):
+        self.title = title
+        self.content = content
+
 
 def get_db_connection():
     conn = db.connect(
@@ -13,8 +26,27 @@ def get_db_connection():
     )
     return conn
 
+# Create one
+@app.route('/', methods=['POST'])
+def create():
+    postModel = request.json
+    if not postModel:
+        return "No model to create", 400
+    else:
+        title = postModel['title']
+        content = postModel['content']
+        post = Post(title,content)
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO posts (title,content) VALUES (%s,%s)", (post.title, post.content))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return "Created"
+
+# Read all
 @app.route('/')
-def index():
+def getAll():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("SELECT * FROM posts")
@@ -26,3 +58,22 @@ def index():
     posts["posts"] = rows
 
     return posts
+
+# Read One
+@app.route('/<post_id>')
+def getOne(post_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM posts WHERE id = %s", post_id)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    posts = {}
+    posts["posts"] = rows
+
+    return posts
+
+# Update one
+
+# Delete one
